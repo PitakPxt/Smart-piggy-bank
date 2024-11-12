bed6d291368f2cbb60cb2efe8ebf862984cff044
Pitak Mennakret <pitakpxt@gmail.com>
2024-11-12 22:58:14 +0700

    Change Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -17,7 +17,6 @@ export default function Profile() {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
-  // ดึงข้อมูลผู้ใช้จาก Firebase
   useEffect(() => {
     if (user?.uid) {
       const userDoc = doc(db, "users", user.uid);
