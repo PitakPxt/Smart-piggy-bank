f648699b8a4a1754e01522688e5975a4f696b647
Pitak Mennakret <pitakpxt@gmail.com>
2024-11-24 11:51:45 +0700

    resposive navbar test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import LogoImage from "@images/logo-pic.svg";
 import HamburgerMenu from "@images/hamburger-menu.svg";
 import FriendPartyModal from "@modals/FriendPartyModal";
 import BtnYellow from "../components/BtnYellow";
+import CloseIcon from "@images/icon-close.svg";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useUserAuth } from "../context/AuthContext";
 import { cn } from "../lib/tailwindcss";
@@ -10,6 +11,7 @@ import { Link } from "react-router-dom";
 import React, { useEffect, useRef, useState } from "react";
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "../lib/firebase";
+import { useScreen } from "@/hooks/useScreen";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -18,6 +20,10 @@ export default function Navbar() {
   const modalRef = useRef(null);
   const [userPin, setUserPin] = useState(null);
   const [showNavItems, setShowNavItems] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = useLocation().pathname;
+
+  const { isMobile } = useScreen();
 
   useEffect(() => {
     const fetchAndWatchUserPin = async () => {
@@ -54,6 +60,10 @@ export default function Navbar() {
     };
   }, []);
 
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   const handleShowFriendPartyModal = () => {
     setShowFriendPartyModal(true);
   };
@@ -79,27 +89,81 @@ export default function Navbar() {
     <>
       <div className="w-full flex pt-[20px] pb-[16px] items-center justify-between">
         <Link to="/home" className="flex items-center gap-[6px]">
-          <img src={LogoImage} alt="" className="size-[88px]" />
-          <h1 className="text-h3-bold">Smart Piggy Bank</h1>
+          <img
+            src={LogoImage}
+            alt=""
+            className="lg:size-[88px] md:size-[64px] size-[48px]"
+          />
+          <h1 className="text-h4-bold lg:text-h1-bold">Smart Piggy Bank</h1>
         </Link>
 
-        <div className="flex gap-5 text-h3-bold">
+        <div className="flex gap-1 lg:gap-5 items-center text-h4-bold lg:text-h3-bold">
           {user && showNavItems && (
             <>
-              <NavbarItem text="ปลดล็อค" to="/unlock-pin" />
-              <NavbarItem text="สร้างปาร์ตี้" to="/create-party" />
-              <div role="button" className="px-4 py-2">
-                <div className="relative" ref={modalRef}>
-                  <span
-                    onClick={handleShowFriendPartyModal}
-                    className="text-secondary-300"
+              {!isMobile ? (
+                <>
+                  <NavbarItem text="ปลดล็อค" to="/unlock-pin" />
+                  <NavbarItem text="สร้างปาร์ตี้" to="/create-party" />
+                  <div role="button" className="px-4 py-2">
+                    <div className="relative" ref={modalRef}>
+                      <span
+                        onClick={handleShowFriendPartyModal}
+                        className="text-secondary-300"
+                      >
+                        คอมมูนิตี้
+                      </span>
+                      {showFriendPartyModal && <FriendPartyModal />}
+                    </div>
+                  </div>
+                  <NavbarItem text="โปรไฟล์" to="/profile" />
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setIsOpen(true)} className="px-4 py-2">
+                    <div className="relative" ref={modalRef}>
+                      <img src={HamburgerMenu} alt="" className="size-[36px]" />
+                    </div>
+                  </button>
+
+                  <div
+                    className={cn(
+                      "fixed inset-0 z-50 transition-transform duration-300 translate-x-full flex justify-end backdrop-blur-[2px]",
+                      isOpen && "translate-x-0"
+                    )}
                   >
-                    คอมมูนิตี้
-                  </span>
-                  {showFriendPartyModal && <FriendPartyModal />}
-                </div>
-              </div>
-              <NavbarItem text="โปรไฟล์" to="/profile" />
+                    <div className="max-w-[246px] w-full bg-primary-100 rounded-lg shadow-main-shadow ">
+                      <div className="flex flex-col gap-2 w-full h-full justify-between">
+                        <div className="flex flex-col">
+                          <div className="flex justify-end pt-[50px] pr-[28px] pb-[20px]">
+                            <button onClick={() => setIsOpen(false)}>
+                              <img
+                                src={CloseIcon}
+                                alt=""
+                                className="size-[14px]"
+                              />
+                            </button>
+                          </div>
+                          <NavbarItem text="ปลดล็อค" to="/unlock-pin" />
+                          <NavbarItem text="สร้างปาร์ตี้" to="/create-party" />
+                          <NavbarItem text="คอมมูนิตี้" to="/community" />
+                          <NavbarItem text="โปรไฟล์" to="/profile" />
+                        </div>
+
+                        <div className="flex justify-center gap-2 pb-[17px] items-center">
+                          <img
+                            src={LogoImage}
+                            alt=""
+                            className="lg:size-[28px] md:size-[28px] size-[28px]"
+                          />
+                          <h1 className="text-h5-bold lg:text-h1-bold">
+                            Smart Piggy Bank
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )}
             </>
           )}
           {!user && (
@@ -120,12 +184,13 @@ export default function Navbar() {
 const NavbarItem = ({ text, className, to }) => {
   const location = useLocation();
   const isActive = location.pathname === to;
+  const { isMobile } = useScreen();
 
   return (
     <Link
       className={cn(
         "px-4 py-2",
-        isActive && "border-b-4 border-primary-500",
+        isActive && !isMobile && "border-b-4 border-primary-500",
         className
       )}
       to={to}
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/AuthContext";
-
+import NotFoundModal from "./modals/NotFoundModal";
 export default function ProtectedRoute({ children }) {
-  const { user } = useUserAuth();
+  const { user, loading } = useUserAuth();
+  if (loading) {
+    return (
+      <NotFoundModal
+        src={"/lottie/loading.lottie"}
+        text="กำลังโหลดข้อมูล..."
+        showBackButton={false}
+      />
+    );
+  }
+
   if (!user) {
-    console.log(user);
     return <Navigate to="/" />;
   }
   return children;
