0809520d20dcd5f5f168b43d182c0ba27986c03e
Pitak Mennakret <pitakpxt@gmail.com>
2024-11-25 16:00:02 +0700

    Responsive Change  Profile

--- src/components/modals/ChangeProfileModal.jsx
+++ src/components/modals/ChangeProfileModal.jsx
@@ -195,7 +195,7 @@ export default function ChangeProfileModal({ onClose, onUpdate }) {
         ) : (
           <div className="xl:w-[540px] xl:h-[800px] lg:w-[540px] lg:h-[720px] md:w-[500px] md:h-[670px] sm:w-[342px] sm:h-[620px] bg-neutral-white-100 rounded-3xl overflow-hidden drop-shadow-lg flex justify-center">
             <div className="flex flex-col md:w-[436px] sm:w-[266px] items-center pt-[42px]">
-              <div className="self-end mr-[42px]" onClick={onClose}>
+              <div onClick={onClose}>
                 <BtnClose />
               </div>
 
