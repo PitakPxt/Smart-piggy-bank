5f4448c93a8745e2f57aeba070781443d6219f48
Pitak Mennakret <pitakpxt@gmail.com>
2024-11-29 20:23:59 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./index.css";
+import "./index.css";]
 
 import Layout from "@components/Layout";
 import Login from "@pages/Login";
@@ -17,7 +17,6 @@ import Ranking from "@pages/Ranking";
 import UnlockSuccess from "@pages/UnlockSuccess";
 import ProtectedRoute from "@components/ProtectedRoute";
 import FriendParty from "@components/modals/FriendPartyModal";
-import Test from "@pages/test";
 
 import AddFriendModal from "@components/modals/AddFriendModal";
 import YourComponent from "@pages/YourComponent";
